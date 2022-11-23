5c6394e7956c52ff17b6e67fb253caea2f840b62
anhhtchy <anhhtchy@gmail.com>
2022-11-23 15:34:26 +0700

    + dps viewer manager

--- src/app/components/common/viewer/viewer-tabs/viewer-tabs.component.ts
+++ src/app/components/common/viewer/viewer-tabs/viewer-tabs.component.ts
@@ -1,23 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { IViewerTab } from 'src/app/models/viewer-tab';
 import { ViewerStateService } from 'src/app/shared/app-state/viewer-state.service';
+import { Constants } from 'src/app/shared/constants/constants';
 
 @Component({
   selector: 'viewer-tabs',
   templateUrl: './viewer-tabs.component.html',
   styleUrls: ['./viewer-tabs.component.scss']
 })
-export class ViewerTabsComponent implements OnInit {
+export class ViewerTabsComponent implements OnInit, OnDestroy {
   protected _currentTabsSubscription: Subscription;
   currentTabs:IViewerTab[] = [];
-  
+  LAYOUT = Constants.LAYOUT;
+  @Input() selectedLayout = Constants.LAYOUT.FULL;
+  currentCaseId = '';
+  protected _currentCaseSubscription: Subscription;
+
   constructor(
     private viewerState: ViewerStateService,
-
   ) { 
     this._currentTabsSubscription = this.viewerState.subscribeCurrentTabs( (tabs: IViewerTab[]) => {
       this.currentTabs = tabs;
+    });
+    this._currentCaseSubscription = this.viewerState.subscribeCurrentCase( (id: string) => {
+      this.currentCaseId = id;
     });
   }
 
@@ -26,9 +33,14 @@ export class ViewerTabsComponent implements OnInit {
 
   public ngOnDestroy(): void {
     this._currentTabsSubscription.unsubscribe();
+    this._currentCaseSubscription.unsubscribe();
   }
 
   closeTab(id: string) {
     this.viewerState.closeTab(id);
+  }
+
+  openTab(tab: IViewerTab) {
+    this.viewerState.openTab(tab);
   }
 }
--- src/app/components/common/viewer/viewer.component.ts
+++ src/app/components/common/viewer/viewer.component.ts
@@ -1,16 +1,93 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnDestroy, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { IViewerTab } from 'src/app/models/viewer-tab';
+import { ViewerStateService } from 'src/app/shared/app-state/viewer-state.service';
 
 @Component({
   selector: 'app-viewer',
   templateUrl: './viewer.component.html',
   styleUrls: ['./viewer.component.scss']
 })
-export class ViewerComponent implements OnInit {
+export class ViewerComponent implements OnInit, OnDestroy {
   @Input() caseStudyId = '';
-
-  constructor() { }
+  protected _currentTabsSubscription: Subscription;
+  currentTabs:IViewerTab[] = [];
+  currentCaseId = '';
+  protected _currentCaseSubscription: Subscription;
+  @ViewChild("viewerContainer") viewerContainer!: ElementRef;
+  renderedCases:string[] = [];
+  constructor(
+    private viewerState: ViewerStateService,
+    private renderer:Renderer2
+  ) { 
+    this._currentTabsSubscription = this.viewerState.subscribeCurrentTabs( (tabs: IViewerTab[]) => {
+      this.currentTabs = tabs;
+    });
+    this._currentCaseSubscription = this.viewerState.subscribeCurrentCase( (id: string) => {
+      this.currentCaseId = id;
+      this.changeCaseStudy(id);
+    });
+  }
 
   ngOnInit(): void {
   }
 
+  public ngOnDestroy(): void {
+    this._currentTabsSubscription.unsubscribe();
+    this._currentCaseSubscription.unsubscribe();
+  }
+
+  changeCaseStudy(id: string) {
+    console.log('changeCaseStudy', id)
+    if (!id) {
+      this.removeViewers();
+    }
+    else if (!this.renderedCases.includes(id)) {
+      this.addNewViewer(id);
+      this.renderedCases.push(id);
+    } else {
+      this.setCurrentViewer(id);
+    }
+  }
+
+  addNewViewer(id: string) {
+    const newContainer = this.renderer.createElement('div');
+    this.renderer.setAttribute(newContainer, 'id', 'case'+id);
+    this.renderer.addClass(newContainer, 'msc-viewer-container');
+    this.renderer.addClass(newContainer, 'w-full');
+    this.renderer.addClass(newContainer, 'h-full');
+    this.renderer.addClass(newContainer, 'relative');
+    this.renderer.addClass(newContainer, 'hidden');
+
+    const newIFrame = this.renderer.createElement('iframe');
+    this.renderer.setAttribute(newIFrame, 'id', '0');
+    this.renderer.setAttribute(newIFrame, 'style', 'border:none;width:100%;top:0;left:0;right:0;bottom:0;position:absolute;height:100%;');
+    this.renderer.setAttribute(newIFrame, 'src', 'https://dpstest2.pmr.vn/dps-viewer?domain=http://dpstest.pmr.vn&slide=63529f473c248e0930065269');
+
+    this.renderer.appendChild(newContainer, newIFrame);
+    this.renderer.appendChild(this.viewerContainer.nativeElement, newContainer);
+    this.setCurrentViewer(id);
+  }
+
+  removeViewers() {
+    let viewers = this.viewerContainer.nativeElement.querySelectorAll('.msc-viewer-container');
+    viewers.forEach((v:any) => {
+      this.renderer.removeChild(this.viewerContainer.nativeElement, v);
+    });
+  }
+
+  setCurrentViewer(id: string) {
+    let viewers = this.viewerContainer.nativeElement.querySelectorAll('.msc-viewer-container');
+    viewers.forEach((v:any) => {
+      if (this.currentTabs.findIndex(t => t.caseStudyId == id) > -1) {
+        this.renderer.addClass(v, 'hidden');
+      } else {
+        this.renderer.removeChild(this.viewerContainer.nativeElement, v);
+      }
+    });
+    let currentViewer = this.viewerContainer.nativeElement.querySelector('#'+'case'+id);
+    if (currentViewer) {
+      this.renderer.removeClass(currentViewer, 'hidden');
+    }
+  }
 }
