198fe42720f2aecfb3ac0c3ad47b6e5ed64cb0b9
anhhtchy <anhhtchy@gmail.com>
2023-01-17 14:34:04 +0700

    - remove getDoctors in ReportActionsComponent

--- src/app/components/common/report/report-actions/report-actions.component.ts
+++ src/app/components/common/report/report-actions/report-actions.component.ts
@@ -1,6 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { UserService } from 'src/app/services/user.service';
-import { Constants, Roles } from 'src/app/shared/constants/constants';
+import { Constants } from 'src/app/shared/constants/constants';
 
 @Component({
   selector: 'report-actions',
@@ -9,46 +8,20 @@ import { Constants, Roles } from 'src/app/shared/constants/constants';
 })
 export class ReportActionsComponent implements OnInit {
   REPORT_ACTIONS = Constants.REPORT_ACTIONS;
-  doctors: any[] = [];
   @Output() onAction = new EventEmitter<any>();
   @Input() report: any = {};
   @Input() disableEditor = true;
   @Input() disableActions = true;
 
   constructor(
-    public userService: UserService,
   ) { }
 
   ngOnInit(): void {
-    this.getDoctors();
   }
 
   emitAction(action: any) {
     this.onAction.emit(
       { action: action }
     );
-  }
-
-  getDoctors() {
-    let payload = {
-      take: 1000,
-      skip: 0,
-      keyword: ''
-    };
-    this.userService.getUserRoles(payload).subscribe({
-      next: (res) => {
-        if (res.isValid) {
-          res.jsonData.data.forEach((d:any) => {
-            if (d.roles && d.roles.includes(Roles.DOCTOR_READ)) {
-              this.doctors.push({
-                username: d.username,
-                fullname: d.fullname,
-                id: d.id
-              });
-            }
-          });
-        }
-      }
-    });
   }
 }
